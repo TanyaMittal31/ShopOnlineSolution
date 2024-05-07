b3dc3e20c6450f3cb5beef1d0e3b6cb8b8fbb06c
Tanya Agrawal <tanyaagrawal2409@gmail.com>
2024-05-07 15:05:58 +0530

    Day1

---
+++ ShopOnline.Api/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShopOnline.Api.Extensions;
+using ShopOnline.Api.Repositories.Contracts;
+using ShopOnline.Models.DTOs;
+
+namespace ShopOnline.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetItems()
+        {
+            try
+            {
+                var products = await this.productRepository.GetItems();
+                var categories = await this.productRepository.GetCategories();
+
+
+                if (products == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    var productDtos = products.ConvertToDto(categories);
+
+                    return Ok(productDtos);
+                }
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                "Error retrieving data from the database");
+
+            }
+        }
+    }
+}
